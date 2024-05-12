3b974858a9a0f3f3801fdd0713a09464c815e3bc
Jan Judas <snugar.i@gmail.com>
2024-05-12 13:15:25 +0200

    part 2, also suboptimal

--- src/year2021/day05.rs
+++ src/year2021/day05.rs
@@ -1,32 +1,38 @@
-use std::ops::RangeInclusive;
+use std::cmp::{max, Ordering};
+use std::iter::successors;
 
 use parse_display::FromStr;
 
 use crate::input::{HashableIteratorExtras, InputData, IteratorYoloParsing};
 
 pub fn part_1(input: &InputData) -> usize {
-    let covered_point_histogram = input.lines().parse_yolo::<Line2D>()
-        .flat_map(|line| line.covered_points_if_simple())
-        .histogram();
-
-    covered_point_histogram.into_values()
+    input.lines().parse_yolo::<Line2D>()
+        .filter(|line| !line.is_diagonal())
+        .flat_map(|line| line.covered_points())
+        .histogram()
+        .into_values()
         .filter(|it| *it >= 2)
         .count()
 }
 
-pub fn part_2(input: &InputData) -> i64 {
-    0
+pub fn part_2(input: &InputData) -> usize {
+    input.lines().parse_yolo::<Line2D>()
+        .flat_map(|line| line.covered_points())
+        .histogram()
+        .into_values()
+        .filter(|it| *it >= 2)
+        .count()
 }
 
-#[derive(FromStr, PartialEq, Eq, Hash)]
+#[derive(FromStr, PartialEq, Eq, Hash, Clone, Copy)]
 #[display("{x},{y}")]
 struct Point2D {
-    x: u64,
-    y: u64,
+    x: i64,
+    y: i64,
 }
 
 impl Point2D {
-    fn new(x: u64, y: u64) -> Self {
+    fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
 }
@@ -39,25 +45,30 @@ struct Line2D {
 }
 
 impl Line2D {
-    pub fn covered_points_if_simple(&self) -> Vec<Point2D> {
-        if self.start.x == self.end.x {
-            range_incl_from_unsorted(self.start.y, self.end.y).map(|y| Point2D::new(self.start.x, y)).collect()
-        } else if self.start.y == self.end.y {
-            range_incl_from_unsorted(self.start.x, self.end.x).map(|x| Point2D::new(x, self.start.y)).collect()
-        } else {
-            Vec::new()
-        }
+    pub fn covered_points(&self) -> Vec<Point2D> {
+        let delta_x = sign(self.end.x - self.start.x);
+        let delta_y = sign(self.end.y - self.start.y);
+        let length = max(self.end.x.abs_diff(self.start.x), self.end.y.abs_diff(self.start.y)) + 1;
+        successors(
+            Some(self.start),
+            |Point2D { x, y }| Some(Point2D::new(x + delta_x, y + delta_y)),
+        )
+            .take(length as usize)
+            .collect()
+    }
+
+    pub fn is_diagonal(&self) -> bool {
+        self.start.x != self.end.x && self.start.y != self.end.y
     }
 }
 
-fn range_incl_from_unsorted(value_1: u64, value_2: u64) -> RangeInclusive<u64> {
-    if value_1 < value_2 {
-        value_1..=value_2
-    } else {
-        value_2..=value_1
+fn sign(value: i64) -> i64 {
+    match value.cmp(&0) {
+        Ordering::Less => -1,
+        Ordering::Equal => 0,
+        Ordering::Greater => 1,
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -76,7 +87,7 @@ mod tests {
     fn part_2_works() {
         let result = part_2(&data());
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 12);
     }
 
     fn data() -> InputData {
