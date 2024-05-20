6e17f6857e75159b868b199e8c3eaf565d342d0c
Jan Judas <snugar.i@gmail.com>
2024-05-20 06:47:02 +0200

    day 8 part 1 with manual parsing

---
+++ src/year2021/day08.rs
@@ -0,0 +1,50 @@
+use crate::input::InputData;
+
+pub fn part_1(input: &InputData) -> usize {
+    input.lines()
+        .map(|line| line.split_once(" | ").unwrap().1)
+        .flat_map(|part| part.split(' '))
+        .filter(|digit| digit.len() != 5 && digit.len() != 6)
+        .count()
+}
+
+pub fn part_2(input: &InputData) -> i64 {
+    0
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::input::InputData;
+
+    use super::*;
+
+    #[test]
+    fn part_1_works() {
+        let result = part_1(&data());
+
+        assert_eq!(result, 26);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = part_2(&data());
+
+        assert_eq!(result, 0);
+    }
+
+    fn data() -> InputData {
+        InputData::from_string("
+            be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+            edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+            fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+            fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+            aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+            fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+            dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+            bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+            egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+            gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+        ")
+    }
+}
