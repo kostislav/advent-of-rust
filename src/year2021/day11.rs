4f1b7e41552bf3c5f1237a57ac718dd58c1b2624
Jan Judas <snugar.i@gmail.com>
2024-06-09 20:14:13 +0200

    day 11 part 2

--- src/year2021/day11.rs
+++ src/year2021/day11.rs
@@ -5,40 +5,55 @@ use crate::input::InputData;
 
 pub fn part_1(input: &InputData) -> usize {
     let mut cavern = Array2d::from_transformed_input(input, |c| c - b'0');
-    let mut queue = VecDeque::with_capacity(100);
-    let mut num_flashes = 0;
-    for _ in 0..100 {
-        cavern.for_each(|octopus, _| queue.push_back(octopus));
+    let mut queue = VecDeque::with_capacity(1000);
+    (0..100)
+        .map(|i| run_step(&mut cavern, &mut queue))
+        .sum()
+}
 
-        while let Some(octopus) = queue.pop_front() {
-            cavern[octopus] += 1;
-            if cavern[octopus] == 10 {
-                num_flashes += 1;
-                for delta_rows in [-1, 0, 1] {
-                    for delta_columns in [-1, 0, 1] {
-                        let neighbor = Coordinate2d::new(octopus.row() + delta_rows, octopus.column() + delta_columns);
-                        if neighbor != octopus && cavern.is_inside(&neighbor) {
-                            queue.push_back(neighbor);
-                        }
+pub fn part_2(input: &InputData) -> i64 {
+    let mut cavern = Array2d::from_transformed_input(input, |c| c - b'0');
+    let mut queue = VecDeque::with_capacity(1000);
+    let mut step = 1;
+    loop {
+        let num_flashes = run_step(&mut cavern, &mut queue);
+
+        if num_flashes == 100 {
+            return step;
+        } else {
+            step += 1;
+        }
+    }
+}
+
+fn run_step(cavern: &mut Array2d<u8>, queue: &mut VecDeque<Coordinate2d>) -> usize {
+    let mut num_flashes = 0;
+    cavern.for_each(|octopus, _| queue.push_back(octopus));
+
+    while let Some(octopus) = queue.pop_front() {
+        cavern[octopus] += 1;
+        if cavern[octopus] == 10 {
+            num_flashes += 1;
+            for delta_rows in [-1, 0, 1] {
+                for delta_columns in [-1, 0, 1] {
+                    let neighbor = Coordinate2d::new(octopus.row() + delta_rows, octopus.column() + delta_columns);
+                    if neighbor != octopus && cavern.is_inside(&neighbor) {
+                        queue.push_back(neighbor);
                     }
                 }
             }
         }
-
-        cavern.map_in_place(|_, &energy_level|
-            if energy_level > 9 {
-                0
-            } else {
-                energy_level
-            }
-        )
     }
 
-    num_flashes
-}
+    cavern.map_in_place(|_, &energy_level|
+        if energy_level > 9 {
+            0
+        } else {
+            energy_level
+        }
+    );
 
-pub fn part_2(input: &InputData) -> i64 {
-    0
+    num_flashes
 }
 
 
@@ -59,7 +74,7 @@ mod tests {
     fn part_2_works() {
         let result = part_2(&data());
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 195);
     }
 
     fn data() -> InputData {
