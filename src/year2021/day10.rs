45fb269301e7236ee9e64c4820e705a664fafff1
Jan Judas <snugar.i@gmail.com>
2024-06-28 20:51:29 +0200

    U8Map

--- src/year2021/day10.rs
+++ src/year2021/day10.rs
@@ -4,20 +4,20 @@ use crate::input::{InputData, OrdIteratorExtras};
 
 pub fn part_1(input: &InputData) -> u64 {
     let closers = closers();
-    let mut points = [0u16; 128];
-    points[b')' as usize] = 3;
-    points[b']' as usize] = 57;
-    points[b'}' as usize] = 1197;
-    points[b'>' as usize] = 25137;
+    let mut points = U8Map::new();
+    points.put(b')', 3);
+    points.put(b']', 57);
+    points.put(b'}', 1197);
+    points.put(b'>', 25137);
     input.lines()
         .filter_map(|line| {
             let mut stack = VecDeque::with_capacity(line.len());
             for &c in line {
-                let closer = closers[c as usize];
+                let closer = closers.get(c);
                 if closer != 0 {
                     stack.push_back(closer);
                 } else if stack.pop_back().unwrap() != c {
-                    return Some(points[c as usize] as u64);
+                    return Some(points.get(c) as u64);
                 }
             }
             None
@@ -27,16 +27,16 @@ pub fn part_1(input: &InputData) -> u64 {
 
 pub fn part_2(input: &InputData) -> u64 {
     let closers = closers();
-    let mut points = [0u8; 128];
-    points[b')' as usize] = 1;
-    points[b']' as usize] = 2;
-    points[b'}' as usize] = 3;
-    points[b'>' as usize] = 4;
+    let mut points = U8Map::new();
+    points.put(b')', 1);
+    points.put(b']', 2);
+    points.put(b'}', 3);
+    points.put(b'>', 4);
     input.lines()
         .filter_map(|line| {
             let mut stack = VecDeque::with_capacity(line.len());
             for &c in line {
-                let closer = closers[c as usize];
+                let closer = closers.get(c);
                 if closer != 0 {
                     stack.push_back(closer);
                 } else if stack.pop_back().unwrap() != c {
@@ -45,19 +45,37 @@ pub fn part_2(input: &InputData) -> u64 {
             }
             Some(
                 stack.iter().rev()
-                    .fold(0u64, |acc, &c| acc * 5 + points[c as usize] as u64)
+                    .fold(0u64, |acc, &c| acc * 5 + points.get(c) as u64)
             )
         })
         .median()
 }
 
-fn closers() -> [u8; 128] {
-    let mut closers = [0u8; 128];
-    closers[b'(' as usize] = b')';
-    closers[b'[' as usize] = b']';
-    closers[b'{' as usize] = b'}';
-    closers[b'<' as usize] = b'>';
+fn closers() -> U8Map<u8> {
+    let mut closers = U8Map::new();
+    closers.put(b'(', b')');
+    closers.put(b'[', b']');
+    closers.put(b'{', b'}');
+    closers.put(b'<', b'>');
     closers
+}
+
+struct U8Map<V> {
+    values: [V; 256],
+}
+
+impl<V: Default + Copy> U8Map<V> {
+    fn new() -> Self {
+        Self { values: [V::default(); 256] }
+    }
+
+    fn put(&mut self, key: u8, value: V) {
+        self.values[key as usize] = value;
+    }
+
+    fn get(&self, key: u8) -> V {
+        self.values[key as usize]
+    }
 }
 
 
