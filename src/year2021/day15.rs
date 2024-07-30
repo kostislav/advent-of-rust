8546114d1abf3777f8e06e508d2807ff83009d72
Jan Judas <snugar.i@gmail.com>
2024-07-30 11:44:16 +0200

    day 15 part 2

--- src/year2021/day15.rs
+++ src/year2021/day15.rs
@@ -8,14 +8,31 @@ pub fn part_1(input: &InputData) -> usize {
     shortest_path(
         Coordinate2d::new(0, 0),
         Coordinate2d::new(cavern.num_rows() as isize - 1, cavern.num_columns() as isize - 1),
-        |point| [(-1, 0), (1, 0), (0, -1), (0, 1)].iter()
-            .map(move |(delta_x, delta_y)| Coordinate2d::new(point.row() + delta_x, point.column() + delta_y))
-            .filter_map(|neighbor| cavern.get(neighbor).map(move |&it| (neighbor, it as usize)))
+        |point| [point.up(), point.down(), point.left(), point.right()].into_iter()
+            .filter_map(|neighbor| cavern.get(neighbor).map(move |&it| (neighbor, it as usize))),
     )
 }
 
-pub fn part_2(input: &InputData) -> i64 {
-    0
+pub fn part_2(input: &InputData) -> usize {
+    let cavern = Array2d::from_transformed_input(input, |c| c - b'0');
+    let num_virtual_rows = (cavern.num_rows() * 5) as isize;
+    let num_virtual_columns = (cavern.num_rows() * 5) as isize;
+
+    shortest_path(
+        Coordinate2d::new(0, 0),
+        Coordinate2d::new(num_virtual_rows - 1, num_virtual_columns - 1),
+        |point| [point.up(), point.down(), point.left(), point.right()].into_iter()
+            .filter_map(|neighbor|
+                if neighbor.row() >= 0 && neighbor.column() >= 0 && neighbor.row() < num_virtual_rows && neighbor.column() < num_virtual_columns {
+                    let tile_offset = neighbor.row() as usize / cavern.num_rows() + neighbor.column() as usize / cavern.num_columns();
+                    let original_risk_value = cavern[Coordinate2d::new(neighbor.row() % (cavern.num_rows() as isize), neighbor.column() % (cavern.num_columns() as isize))] as usize;
+                    let modified_risk_value = original_risk_value + tile_offset;
+                    Some((neighbor, ((modified_risk_value - 1) % 9) + 1))
+                } else {
+                    None
+                }
+            )
+    )
 }
 
 
@@ -36,7 +53,7 @@ mod tests {
     fn part_2_works() {
         let result = part_2(&data());
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 315);
     }
 
     fn data() -> InputData {
